adaa61905c2a2e3206096d8d158a7954f4ab64df
Dev Paliwal <devpaliwal7764@gmail.com>
2026-08-20 14:33:17 +0530

    Route name change

--- resumio/src/pages/Dashboard.jsx
+++ resumio/src/pages/Dashboard.jsx
@@ -121,7 +121,7 @@ const Dashboard = () => {
   return (
     <div>
         <div className='max-w-7xl mx-auto px-4 py-8'>
-            <p className='text-2xl font-medium mb-6 bg-linear-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, ${user?.name}</p>
+            <p className='text-2xl font-medium mb-6 bg-linear-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, {user?.name}</p>
 
             <div className='flex gap-4'>
                 <button onClick={()=>{
