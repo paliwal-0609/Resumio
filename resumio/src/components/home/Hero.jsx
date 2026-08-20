2d71d8a93d1191c4b2c524f9b04d6de8c7bcfca0
Dev Paliwal <devpaliwal7764@gmail.com>
2026-08-20 16:09:47 +0530

    fix small bug

--- resumio/src/components/home/Hero.jsx
+++ resumio/src/components/home/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                 <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
                     <a href="#" className="hover:text-red-600 transition">Home</a>
                     <a href="#features" className="hover:text-red-600 transition">Features</a>
-                    <a href="#testimonials" className="hover:text-red-600 transition">Testimonials</a>
+                    <a href="#testimonial" className="hover:text-red-600 transition">Testimonials</a>
                     <a href="#cta" className="hover:text-red-600 transition">Contact</a>
                 </div>
 
@@ -55,7 +55,7 @@ const Hero = () => {
             <div className={`fixed inset-0 z-100 bg-black/40 text-black backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300 ${menuOpen ? "translate-x-0" : "-translate-x-full"}`} >
                 <a href="#" className="text-white">Home</a>
                 <a href="#features" className="text-white">Features</a>
-                <a href="#testimonials" className="text-white">Testimonials</a>
+                <a href="#testimonial" className="text-white">Testimonials</a>
                 <a href="#cta" className="text-white">Contact</a>
                 <button onClick={() => setMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-red-600 hover:bg-red-700 transition text-white rounded-md flex" >
                     X
