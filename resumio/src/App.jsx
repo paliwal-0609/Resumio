62e246e65376cd1a4d32dd7e6f01811d370ed17f
Dev Paliwal <devpaliwal7764@gmail.com>
2026-08-20 11:35:35 +0530

    ResumeBuilder-Day-12.1(Minor Changes)

--- resumio/src/App.jsx
+++ resumio/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
 
   return (
     <>
-    <Toaster />
+      <Toaster />
       <Routes>
         <Route path='/' element={<Home/>}/>
         
--- resumio/src/components/home/Footer.jsx
+++ resumio/src/components/home/Footer.jsx
@@ -27,40 +27,19 @@ const Footer = () => {
                         <li><a href="/" className="hover:text-red-600 transition">About</a></li>
                     </ul>
                 </div>
-                <div>
-                    <p className="text-slate-800 font-semibold">Legal</p>
-                    <ul className="mt-2 space-y-2">
-                        <li><a href="/" className="hover:text-red-600 transition">Privacy</a></li>
-                        <li><a href="/" className="hover:text-red-600 transition">Terms</a></li>
-                    </ul>
-                </div>
             </div>
             <div className="flex flex-col max-md:items-center max-md:text-center gap-2 items-end">
                 <p className="max-w-60">Making every customer feel valued—no matter the size of your audience.</p>
                 <div className="flex items-center gap-4 mt-3">
-                    <a href="https://www.linkedin.com/company/prebuiltui" target="_blank" rel="noreferrer">
+                    <a href="https://www.linkedin.com/in/dev-paliwal-08b2b81a3/" target="_blank" rel="noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin size-5 hover:text-red-500" aria-hidden="true">
                             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                             <rect width="4" height="12" x="2" y="9"></rect>
                             <circle cx="4" cy="4" r="2"></circle>
                         </svg>
                     </a>
-                    <a href="https://x.com/prebuiltui" target="_blank" rel="noreferrer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter size-5 hover:text-red-500" aria-hidden="true">
-                            <path
-                                d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z">
-                            </path>
-                        </svg>
-                    </a>
-                    <a href="https://www.youtube.com/@prebuiltui" target="_blank" rel="noreferrer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-youtube size-6 hover:text-red-500" aria-hidden="true">
-                            <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17">
-                            </path>
-                            <path d="m10 15 5-3-5-3z"></path>
-                        </svg>
-                    </a>
                 </div>
-                <p className="mt-3 text-center">© 2026 <a href="https://prebuiltui.com">Resumio</a></p>
+                <p className="mt-3 text-center">© 2026 <a href="/">Resumio</a></p>
             </div>
         </footer>
 
